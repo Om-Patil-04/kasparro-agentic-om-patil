e64a2f307cc70d79a7315106afe47fc42702b019
Om Patil <ompatil4529@gmail.com>
2025-12-28 19:16:35 +0530

    add static Product B config for comparisons

---
+++ product_b_config.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+PRODUCT_B = {
+    "name": "LumiCare Bright Serum",
+    "price": "₹649",
+    "key_ingredients": ["Niacinamide 5%", "Licorice Extract"],
+    "benefits": ["Brightening", "Evens tone"],
+    "skin_types": ["Normal", "Combination"],
+}
